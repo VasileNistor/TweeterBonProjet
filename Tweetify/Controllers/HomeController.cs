e7d8e68e20d34ed5658225907a64b6012c022940
Vasile NISTOR <vasile.nistor@estiam.com>
2019-03-19 10:37:51 +0100

    connexion, inscription ok

---
+++ Tweetify/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tweetify.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tweetify.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tweetify.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (!HttpContext.Session.GetInt32("UserId").HasValue)
+            {
+                return RedirectToAction("Login", "Auth");
+            }
+            else
+            {
+                using (var context = new TweetifyContext())
+                {
+                    var t = context.Tweets.Include("Author").ToList();
+
+                    ViewData["tweetbox"] = t;
+                }
+            }
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
